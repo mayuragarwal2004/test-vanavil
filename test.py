4dfd2059d4a40ec1d2916e4ef9c8296cf74b1fed
Mayur Agarwal <mayur.m.agarwal@gmail.com>
2024-10-15 20:15:43 +0530

    new pattern

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
 # code to print star pattern
 n = 5
-k = 2 * n - 2
 for i in range(0, n):
-    for j in range(0, k):
-        print(end=" ")
-    k = k - 1
     for j in range(0, i + 1):
-        print("* ", end="")
-    print("")
+        print("*", end=' ')
+    print("\r")
